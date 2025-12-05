54d7d04d164cd920118aff6dae51d729ee843690
tamerlan1wow <slatergt228@gmail.com>
2025-12-06 01:29:32 +0500

    remove prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@sidebase/nuxt-auth',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate',
-    '@prisma/nuxt',
+    
   ],
   auth: {
     globalAppMiddleware: true
