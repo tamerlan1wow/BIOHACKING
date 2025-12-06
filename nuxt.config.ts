a0986d86acdfefe3a69aacdaa9fb86f14a53fae9
tamerlan1wow <slatergt228@gmail.com>
2025-12-06 12:16:26 +0500

    Create netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  // 👉 ВАЖНО: Netlify НЕ поддерживает SSR Nuxt → нужен SPA
+  ssr: false,
+
+  // 👉 ВАЖНО: Говорим Nuxt генерировать полностью статический сайт
+  nitro: {
+    preset: 'netlify-static'
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
   ],
-  
+
+  // Можно удалить, если GitHub Auth не используется
   runtimeConfig: {
     public: {
       GITHUB_ID: process.env.GITHUB_ID,
-      GITHUB_SECRET: process.env.GITHUB_SECRET
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
     },
   },
 })
