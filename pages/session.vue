8e09cbcec77078893a873e43590f3518f31468f0
tamerlan1wow <slatergt228@gmail.com>
2025-12-06 11:56:16 +0500

    Clean index.vue and remove unused logic

---
+++ pages/session.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-[#0F1114] text-white">
+    <h1 class="text-3xl opacity-50">This page is not used anymore</h1>
+  </div>
+</template>
